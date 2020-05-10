43d65fe30d0309b373bed47ec16cb4d1f7ef1c1e
Jonathan Donaldson <github@cdn.fail>
2020-05-10 19:06:52 +0100

    Added tests and updated test script in github

---
+++ scripts/dockerTest.sh
@@ -0,0 +1,14 @@
+  
+#!/usr/bin/env bash
+
+set -xe
+
+LAUNCH_COMMAND=$(cat runTest.sh)
+
+docker run \
+--rm \
+--user $(id -u):$(id -g) \
+-v $(pwd):/torrentRouter \
+-e TERM=xterm \
+-e HOME=/torrentRouter \
+rust /bin/bash -c "$LAUNCH_COMMAND"
--- src/config.rs
+++ src/config.rs
@@ -19,3 +19,44 @@ pub fn get_watch_directory() -> String {
 pub fn get_dead_letter_directory() -> String {
 	get_required_config("DEAD_LETTER_DIR")
 }
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	#[should_panic]
+	fn should_panic_on_invalid_config() {
+		env::remove_var("VALID_ENV_VAR");
+		get_required_config("INVALID_ENV_VAR");
+	}
+
+	#[test]
+	#[should_panic]
+	fn should_panic_on_watch_directory() {
+		env::remove_var("WATCH_DIR");
+		get_watch_directory();
+	}
+
+	#[test]
+	#[should_panic]
+	fn should_panic_on_dead_letter_directory() {
+		env::remove_var("DEAD_LETTER_DIR");
+		get_dead_letter_directory();
+	}
+	#[test]
+	fn should_get_watch_directory() {
+		env::set_var("WATCH_DIR", "ENV_VALUE");
+		assert_eq!(get_watch_directory(), "ENV_VALUE");
+	}
+	#[test]
+	fn should_get_dead_letter_directory() {
+		env::set_var("DEAD_LETTER_DIR", "ENV_VALUE");
+		assert_eq!(get_dead_letter_directory(), "ENV_VALE");
+	}
+	#[test]
+	fn should_get_valid_required_config() {
+		env::set_var("VALID_ENV_VAR", "ENV_VALUE");
+		assert_eq!(get_required_config("VALID_ENV_VAR"), "ENV_VALUE");
+	}
+}
